c12c8abbcf233dc695e90a3fa7d99058954541f7
jordiisepic <66235907+jordiisepic@users.noreply.github.com>
2020-10-15 08:04:57 -0400

    Redesign

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-  function myFunction() {
-  var x = document.getElementById("mysidebar");
-  if (x.className === "sidebar") {
-    x.className += " responsive";
-  } else {
-    x.className = "sidebar";
-  }
-}
